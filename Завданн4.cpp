021fd8014d813814a9e0b1fb723b9cfc99ecdb10
Viktoriia2410 <vikyyyysik.14@gmail.com>
2025-09-21 23:41:13 +0300

    Update Завданн4.cpp
    Виконано Завдання 4

--- Завданн4.cpp
+++ Завданн4.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    double -;
+    double x, y;
     cout << "Введіть x: ";
     cin >> x;
 
-    y = --
+    y = 2 * x + 3
 
     cout << "y = " << y << endl;
     return 0;
